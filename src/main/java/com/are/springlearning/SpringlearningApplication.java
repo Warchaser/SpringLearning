0e3bc672a020f43dec425d5194532ae750d8f10b
Achan <wushengbo@ihmair.cn>
2019-09-17 17:22:12 +0800

    fix # Deployed configuration. Signed-off-by: Achan <wushengbo@ihmair.cn>

--- src/main/java/com/are/springlearning/SpringlearningApplication.java
+++ src/main/java/com/are/springlearning/SpringlearningApplication.java
@@ -2,9 +2,16 @@ package com.are.springlearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringlearningApplication {
+public class SpringlearningApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringlearningApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringlearningApplication.class, args);
