6bb424693652f76d0ff1fad11d923ff1ab3a8559
Achan <wushengbo@ihmair.cn>
2019-09-18 15:53:06 +0800

    fix # Access swagger-ui.html with 404. Signed-off-by: Achan <wushengbo@ihmair.cn>

--- src/main/java/com/are/springlearning/Swagger2Configuration.java
+++ src/main/java/com/are/springlearning/Swagger2Configuration.java
@@ -3,6 +3,9 @@ package com.are.springlearning;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.context.support.WebApplicationObjectSupport;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class Swagger2Configuration {
+public class Swagger2Configuration extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket createApi(){
@@ -32,4 +35,21 @@ public class Swagger2Configuration {
                 .version("1.0")
                 .build();
     }
+
+    /**
+     * 防止@EnableMvc把默认的静态资源路径覆盖了，手动设置的方式
+     *
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 解决静态资源无法访问
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        // 解决swagger无法访问
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        // 解决swagger的js文件无法访问
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
+
 }
